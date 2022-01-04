8b03bcc22754b0eaef5f7b9545f9d6796ccb220e
Wanderley Ferreira de Albuquerque <wanderleyfa@gmail.com>
2022-01-04 08:45:29 -0300

    criando testes para um todo

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/todos/{todo}', 'App\Http\Controllers\TodoController@show');
+Route::post('/todos', 'App\Http\Controllers\TodoController@create');
+Route::put('/todos/{todo}', 'App\Http\Controllers\TodoController@update');
+Route::delete('/todos/{todo}', 'App\Http\Controllers\TodoController@delete');
